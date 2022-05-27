5a14730d58c01dcd7e8ca06c210f4daf3ea11801
muhashil <muh.ashil@yahoo.com>
2022-05-27 13:17:13 +0700

    update settings db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BACKEND_BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = BACKEND_BASE_DIR.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,8 +69,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'siapujian_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '13.215.204.73',
+        'PORT': '5432',
     }
 }
 
@@ -111,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
