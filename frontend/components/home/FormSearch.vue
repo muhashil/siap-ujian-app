f483470761b2bdbe6c673ac018ce6549d8475ce7
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-05-31 22:57:19 +0700

    add banner

---
+++ frontend/components/home/FormSearch.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    b-input-group
+      b-form-input(type="text" placeholder="Cari Materi...")
+      b-input-group-append
+        b-input-group-text.bg-white
+          BIconSearch
+</template>
+
+<script>
+export default {
+  name: "FormSearch"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,21 @@
-<template>
-  <Tutorial/>
+<template lang="pug">
+  div.container(style="max-width: 500px;")
+    TopNavbar
+    FormSearch(style="margin-top: 100px;")
+    div.mt-4
+      h6
+        b Pengumuman
+      BannerCarousel
+    //Tutorial
 </template>
 
 <script>
+import TopNavbar from "../components/header/TopNavbar";
+import {BIconSearch} from 'bootstrap-vue'
+import FormSearch from "../components/home/FormSearch";
+import BannerCarousel from "../components/home/BannerCarousel";
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {BannerCarousel, FormSearch, TopNavbar, BIconSearch}
 }
 </script>
