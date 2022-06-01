fb22be95bc35e25618c5ff670946a9f0962f9ca6
muhashil <muh.ashil@yahoo.com>
2022-06-01 13:30:48 +0700

    add authentication api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from backend.api.views_helper import (banner, materi)
 
 app_name = 'api'
 
 urlpatterns = [
+
+    # Auth
+    path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Banner
     path('banners', banner.ListBannerView.as_view()),
 
     # Materi
