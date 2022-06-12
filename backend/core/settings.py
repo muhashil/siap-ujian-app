53400dc08b86deed1d81a6982c42714b073a4ef4
muhashil <muh.ashil@yahoo.com>
2022-06-12 14:22:36 +0700

    add cors settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'backend.core',
     'backend.api'
 ]
@@ -45,6 +47,7 @@ AUTH_USER_MODEL = 'core.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -149,3 +152,5 @@ USE_REMOTE_STORAGE = True
 UPLOADER_SERVICE_URL = env('UPLOADER_SERVICE_URL')
 UPLOADER_SERVICE_TOKEN = env('UPLOADER_SERVICE_TOKEN')
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['*']
